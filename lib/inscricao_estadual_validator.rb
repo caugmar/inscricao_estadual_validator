a57721f9bb319936f3e3f6eafb293a60c2b87b62
Guto Marcicano <caugmar@gmail.com>
2011-08-11 10:23:03 -0300

    Código portado do antigo plugin validates_as_inscricao_estadual.

--- lib/inscricao_estadual_validator.rb
+++ lib/inscricao_estadual_validator.rb
@@ -2,6 +2,12 @@ class InscricaoEstadualValidator < ActiveModel::EachValidator
   autoload :InscricaoEstadual, 'inscricao_estadual_validator/inscricao_estadual'
 
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, options[:message]) unless InscricaoEstadual.valid?(value)
+      return if value.blank?
+      valido = false
+      ValidacaoInscricaoEstadual.estados.each do |estado|
+          valido = eval("ValidacaoInscricaoEstadual::#{estado}.new(estado, value).valido?")
+          break if valido 
+      end
+      record.errors.add(attribute, options[:message]) unless valido
   end
 end
--- lib/inscricao_estadual_validator/inscricao_estadual.rb
+++ lib/inscricao_estadual_validator/inscricao_estadual.rb
@@ -1,53 +1,106 @@
-class InscricaoEstadualValidator::InscricaoEstadual
-  def self.valid?(number)
-    new(number).valid?
-  end
+module ValidacaoInscricaoEstadual
+    class Base
+        attr_reader :pesos
+        attr_reader :cadastro
+        attr_reader :cadastro_tamanho
+        attr_reader :digitos_tamanho
 
-  attr_reader :number
+        def initialize(classe, cadastro)
+            begin 
+                @cadastro = cadastro.scan(/[0-9]/).collect{|x| x.to_i}
+                @uf = classe
 
-  def initialize(number)
-    @number = number.to_s
-  end
+                setup
+                calcular
 
-  def digits
-    @digits ||= number.scan(/\d/).map(&:to_i)
-  end
+            rescue LoadError
+                puts "Classe nao encontrada"
+                exit
+            end
+        end
 
-  def plain
-    @plain ||= digits.join
-  end
+        private
+        def validar
+            acertos = 0
+            1.upto(@digitos_tamanho) do |i|
+                eval <<-EOF
+        if @digito#{i}.to_i == @cadastro[@cadastro.length-i].to_i 
+          acertos += 1
+        end
+                EOF
+            end
 
-  def valid?
-    formatted? && !black_listed? && digits_matches?
-  end
+            acertos == @digitos_tamanho
+        end
 
-  private
+        private
+        def validar_formato
+            if @cadastro.to_s.length > @cadastro_tamanho
+                puts "Numero de inscricao estadual muito grande => #{@uf}" if @uf == "AC"
+                return false
+            end
+            return true
+        end
 
-  def formatted?
-    plain =~ /^\d{14}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/
-  end
+        private
+        def multiplicar_pesos_com_numeros_cadastro(cadastro = @cadastro.clone, pesos = @pesos, tamanho = @digitos_tamanho)
+            soma = 0
+            cadastro = cadastro.clone
 
-  def black_listed?
-    plain =~ /^(\d)\1{13}$/
-  end
+            0.upto(cadastro.length - tamanho) do |i|
+                soma += pesos[i].to_i * cadastro[i].to_i
+            end
 
-  def digits_matches?
-    digit_matches?(12) && digit_matches?(13)
-  end
+            soma
+        end
 
-  def digit_matches?(digit)
-    factor = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
-    factor.unshift 6 if digit == 13
-
-    sum = 0
-
-    digit.times do |i|
-      sum += digits[i] * factor[i]
+        public
+        def valido?
+            #  print "testando #{@uf} => " if @uf == "AC"
+            if validar_formato
+                #     print "passo" if @uf == "AC"
+                #      puts if @uf == "AC"
+                return validar
+            else
+                #       print "nao" if @uf == "AC"
+                #        puts if @uf == "AC"
+                return false
+            end
+        end
     end
 
-    result = sum % 11
-    result = result < 2 ? 0 : 11 - result
+    def self.estados
+        @estados ||= []
+    end
 
-    result == digits[digit]
-  end
+    def self.registrar_estado(estado)
+        estados.push(estado) if not estados.include?(estado)
+    end
 end
+
+require 'estados/AC'
+require 'estados/AL'
+require 'estados/AP'
+require 'estados/BA'
+require 'estados/CE'
+require 'estados/DF'
+require 'estados/ES'
+require 'estados/GO'
+require 'estados/MA'
+require 'estados/MT'
+require 'estados/MG'
+require 'estados/PA'
+require 'estados/PB'
+require 'estados/PR'
+require 'estados/PE'
+require 'estados/PI'
+require 'estados/RJ'
+require 'estados/RN'
+require 'estados/RS'
+require 'estados/RO'
+require 'estados/RR'
+require 'estados/SC'
+require 'estados/SP'
+require 'estados/SE'
+require 'estados/TO'
+
